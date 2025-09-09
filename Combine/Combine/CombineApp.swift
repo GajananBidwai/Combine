5b730723a9d770640c75e4ac414edf893bce28d7
Gajanan Bidwai <gajananbidwai525@gmail.com>
2025-09-09 19:27:48 +0530

    Initial Commit
    Initial commit for combine framework

---
+++ Combine/Combine/CombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineApp.swift
+//  Combine
+//
+//  Created by Neosoft on 09/09/25.
+//
+
+import SwiftUI
+
+@main
+struct CombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
