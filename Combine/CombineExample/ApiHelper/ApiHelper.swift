4f0a7c51e4061d893d9bb373f284158de5422f23
Gajanan Bidwai <gajananbidwai525@gmail.com>
2025-09-11 17:12:13 +0530

    Added Comments
    Added comments for better understanding of components of combine framework.

--- Combine/CombineExample/ApiHelper/ApiHelper.swift
+++ Combine/CombineExample/ApiHelper/ApiHelper.swift
@@ -19,8 +19,11 @@ enum ApiError: Error {
 class ApiHelper{
     
     static let shared = ApiHelper()
+    //Cancel the api call when call view did disappear and also used to avoid memory leak and also used to avoid long runnig task.
     var cancellables: Set<AnyCancellable> = []
     
+    //Future is also a publisher which emits the response of the api over time
+    //Future send the response(OUTPUT) back or error.
     func getData<T: Codable>(url: String, type: T.Type) -> Future<[T], Error> {
         return Future<[T], Error> { [weak self] promise in
             guard let self = self else { return }
@@ -29,8 +32,9 @@ class ApiHelper{
                 promise(.failure(ApiError.invalidUrl))
                 return
             }
-            
+            //Datataskpublisher is the publisher which emits value
             URLSession.shared.dataTaskPublisher(for: apiUrl)
+                //Try map is a operators which Check the response is valid or not
                 .tryMap { (data, response) -> Data in
                     guard let apiResponse = response as? HTTPURLResponse,
                           200...299 ~= apiResponse.statusCode else {
@@ -38,8 +42,11 @@ class ApiHelper{
                     }
                     return data
                 }
+            //Decode is the Operators for decode the response
                 .decode(type: [T].self, decoder: JSONDecoder())
+            //.receives is a scheduler which assign the response to main queue
                 .receive(on: RunLoop.main)
+            //.sink is a Subscriber which get the values from the publisher and handle both the closures
                 .sink(
                     receiveCompletion: { completion in
                         if case let .failure(error) = completion {
@@ -57,6 +64,7 @@ class ApiHelper{
                         promise(.success(value))
                     }
                 )
+            //.store is used to handle the cancellables for to avoid memory leak and also stop api call when we call view did disappear
                 .store(in: &self.cancellables)
         }
     }
